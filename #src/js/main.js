8d1d74378046b108ca9180faa0236df0698c92b3
Sergii Tytarenko <tytarenko.sergii@gmail.com>
2021-05-15 19:07:30 +0300

    first commit

---
+++ #src/js/main.js
@@ -0,0 +1,10 @@
+import lightgallery from './vendors/light_gallery';
+
+
+ /* Main
+ ---------------------------------------------------------------*/
+function showText(text = 'no text') {
+    console.log(text);
+}
+showText('last');
+
