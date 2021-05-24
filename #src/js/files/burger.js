93a42a7dba72bd309d57ec66b02fe7050bdf4dd5
Sergii Tytarenko <tytarenko.sergii@gmail.com>
2021-05-24 20:03:18 +0300

    ..header

--- #src/js/files/burger.js
+++ #src/js/files/burger.js
@@ -3,8 +3,8 @@
 const body = document.querySelector('body'),
       mainHeader = document.querySelector('.main-header'),
       burger = document.querySelector('.burger');
-    //   burgerNav = document.querySelector('.burger-nav'),
-    //   burgerNavLink = burgerNav.querySelectorAll('.link');
+      burgerNav = document.querySelector('.main-nav'),
+      burgerNavLink = mainHeader.querySelectorAll('a');
 
 
 /* Burger active
@@ -24,26 +24,26 @@ burger.addEventListener('click', function () {
 
 /* Close menu when links is active
 -----------------------------------------------------------------------------*/
-// for (let i = 0; i < burgerNavLink.length; i++) {
-//     burgerNavLink[i].addEventListener("click", function() {
-//         if(burgerNav.classList.contains('active')) {
-//             closeBurgerNav ();
-//         }
-//     });
-// }
+for (let i = 0; i < burgerNavLink.length; i++) {
+    burgerNavLink[i].addEventListener("click", function() {
+        if(burgerNav.classList.contains('active')) {
+            closeBurgerNav ();
+        }
+    });
+}
 
 
 /* Functions of burger nav
 -----------------------------------------------------------------------------*/
 function showBurgerNav () {
-    // burgerNav.classList.add('active');
+    burgerNav.classList.add('active');
     mainHeader.classList.add('active');
     body_lock(0);
 }
 
 function closeBurgerNav () {
     burger.classList.remove('active');
-    // burgerNav.classList.remove('active');
+    burgerNav.classList.remove('active');
     mainHeader.classList.remove('active');
     body_lock(0);
 }
